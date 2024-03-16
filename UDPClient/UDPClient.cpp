770cc27af801d027cf0af0f0aa69a1dc7051d6ad
dimabalawov <dimabalawov@gmail.com>
2024-03-16 12:53:19 +0200

    Добавьте файлы проекта.

---
+++ UDPClient/UDPClient.cpp
@@ -0,0 +1,72 @@
+#pragma comment(lib, "Ws2_32.lib")
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    WSADATA wsadata;
+
+    int res = WSAStartup(MAKEWORD(2, 2), &wsadata);
+    if (res != NO_ERROR)
+    {
+        cout << "WSAStartup failked with error " << res << endl;
+        return 1;
+    }
+
+    SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (udpSocket == INVALID_SOCKET)
+    {
+        cout << "socket failed with error " << WSAGetLastError() << endl;
+        return 2;
+    }  
+
+    sockaddr_in addrTo;
+    addrTo.sin_family = AF_INET;
+    addrTo.sin_port = htons(23000);
+    inet_pton(AF_INET, "127.0.0.1", &addrTo.sin_addr.s_addr);
+
+    cout << "Enter login and colour code(example: 14:dima): \n" ;
+    char loginBuf[1024]{};
+    cin >> loginBuf;
+    const size_t sendBufSize = 1024;
+    char sendBuf[sendBufSize] = "";
+    cout << "Enter the message: \n";
+    cin >> sendBuf;
+
+    strcat_s(loginBuf, ": ");
+    strcat_s(loginBuf,sendBuf);
+
+    cout << "Sending data...." << endl;
+    int sendResult = sendto(udpSocket, loginBuf, strlen(loginBuf), 0, (SOCKADDR*)&addrTo, sizeof(addrTo));
+    if (sendResult == SOCKET_ERROR)
+    {
+        cout << "sendto failed with error " << WSAGetLastError() << endl;
+        return 4;
+    }
+
+    const size_t receiveBufSize = 1024;
+    char receiveBuf[receiveBufSize];
+
+    sockaddr_in addrFrom;
+    int addrFromSize = sizeof(addrFrom);
+
+    cout << "Receiving data...." << endl;
+    int bytesReceived = recvfrom(udpSocket, receiveBuf, receiveBufSize, 0, (SOCKADDR*)&addrFrom, &addrFromSize);
+
+    if (bytesReceived == SOCKET_ERROR)
+    {
+        cout << "recvfrom failed with error " << WSAGetLastError() << endl;
+        return 4;
+    }
+
+    receiveBuf[bytesReceived] = '\0';
+    cout << "Received from " << addrFrom.sin_addr.s_host << endl;
+    cout << "Data: " << receiveBuf << endl;
+
+    closesocket(udpSocket);
+    WSACleanup();
+}
+
